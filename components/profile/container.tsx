0a941ab291f17f3f98e981080ccbdae0f15a220a
taikou-kobayashi-mg <taiko.kobayashi@m-gild.com>
2022-07-23 20:19:44 +0900

    change:css

--- components/profile/container.tsx
+++ components/profile/container.tsx
@@ -1,10 +1,10 @@
-import { Container } from "@chakra-ui/react";
-import { ProfileEdit } from "./edit/edit";
+import { Container, Heading } from '@chakra-ui/react';
+import { ProfileEdit } from './edit/edit';
 
 export const ProfileContainer = () => {
   return (
-    <Container maxW={1080}>
-      <h1>プロフィール編集</h1>
+    <Container>
+      <Heading>プロフィール編集</Heading>
       <ProfileEdit />
     </Container>
   );
